c511e505b8f46de25894470c83eacd7af51a0fa2
Tarkers <anime1314520@gmail.com>
2022-04-09 22:30:14 +0800

    04-09-10-30-14

--- deploy.ps1
+++ deploy.ps1
@@ -3,12 +3,12 @@ Rename-Item -Path "C:\Users\bigsh\Desktop\Github\Server_Test\Back_up\db.json" -N
 # Move-Item -Path C:\Users\bigsh\Downloads\db.json C:\Users\bigsh\Desktop\Github\Server_Test\public\db.json
 # new data db
 node autoScratch.js
-#for github
+# #for github
 git add .
 git commit -m "$($(Get-Date -f MM-dd-hh-mm-ss))"
 git push
-#for heroku
-heroku git:remote -a dear-family-server
+# #for heroku
+# heroku git:remote -a dear-family-server
 git add .
 git commit -m "$($(Get-Date -f MM-dd-hh-mm-ss))"
 git push --force heroku
