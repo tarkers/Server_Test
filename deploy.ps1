6aec4077e9075d517352918d346fce63fd4036a4
Tarkers <anime1314520@gmail.com>
2022-04-09 12:31:16 +0800

    04-09-12-31-16

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,12 @@
+Move-Item -Path C:\Users\bigsh\Desktop\Github\Server_Test\public\db.json C:\Users\bigsh\Desktop\Github\Server_Test\Back_up\db.json
+Rename-Item -Path "C:\Users\bigsh\Desktop\Github\Server_Test\Back_up\db.json" -NewName "db_$(Get-Date -f MM-dd-hh-mm-ss).txt"
+Move-Item -Path C:\Users\bigsh\Downloads\db.json C:\Users\bigsh\Desktop\Github\Server_Test\public\db.json
+#for github
 git add .
 git commit -m "$($(Get-Date -f MM-dd-hh-mm-ss))"
 git push
+#for heroku
+heroku git:remote -a dear-family-server
+git add .
+git commit -m "$($(Get-Date -f MM-dd-hh-mm-ss))"
+git push --force heroku
