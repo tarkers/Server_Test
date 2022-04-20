5d1b5ee9c68fcc828588919913fcf769c2b3c854
Tarkers <anime1314520@gmail.com>
2022-04-20 21:14:57 +0800

    04-20-09-14-57

--- autoScratch.js
+++ autoScratch.js
@@ -3,6 +3,7 @@ const  fs = require('fs');
 axios.get('https://dear-family-server.herokuapp.com/db.json', {responseType: 'blob'}).then(response => {
     // console.log(response.data)
     fs.writeFile('public/db.json', JSON.stringify(response.data), (err) => {
+        console.log(response.data)
         if (err) throw err;
         console.log('The file has been saved!');
     });
